622d22920529604aac13a6f4d758000fd09d82aa
shenhaoqiang <774779341@qq.com>
2022-03-10 08:43:37 +0800

    📃 docs:测试使用stash

--- test.js
+++ test.js
@@ -1,2 +1,9 @@
 console.log("测试")
 console.log("这是feature-change的内容")
+// 使用git stash
+console.log("这是feature-change更改的内容，但不能commit，我先暂存下")
+// 再使用下git stash save：保存当前未commit的代码并添加备注
+console.log("这是未commit的内容，需要使用git stash save")
+
+
+console.log("我是另一场未commit的，也git stash下")
